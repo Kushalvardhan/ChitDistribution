44d31fe4f22e027376772fceef4000e3b9b54992
Jagadeesh P <jagadeesh.p@karza.in>
2021-08-05 12:21:43 +0530

    our first commit is to push wrong commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ChitDistribution
 Hello world! (description)
+
+
+This line is being added from terminal by opening README.md file!
---
+++ sample.py
@@ -0,0 +1,13 @@
+def isLeap(year):
+    pass
+
+def isPrime(year):
+    pass
+
+
+if __name__ == "main":
+    year = input()
+    if isPrime(year) and isLeap(year):
+        print("Good number")
+    else:
+        print("Bad number")
